7441ddd9569d9712bfdf832152b6ffcabf547982
abdukader <seckader.pro@gmail.com>
2024-05-09 22:40:39 +0000

    Configuration de la base de données

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,10 @@
 /** Todo-list App */
 import * as dotenv from 'dotenv';
+import { AppDataSource } from './config/datasource';
 
 dotenv.config();
 
-console.log(`Hello Todo-list`);
+AppDataSource.initialize().then(() => {
+    console.log('Connexion à la base de données réussi.');
+
+}).catch((error) => console.error(`Erreur de connexion à la base de données ${error}.`));
